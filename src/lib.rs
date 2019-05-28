582f68c4c8b9646a484bc9bc4ed057fd53686ff2
charles-r-earp <charles.r.earp@gmail.com>
2019-05-28 11:48:19 -0700

    Added new cpp_class! wrappers to lib.rs . Troublshooting .travis.yml, build uses wrong repo for some reason.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,24 @@
 use cpp::*;
 
 cpp!{{
+  #include <memory>
   #include <plaidml/plaidml++.h>
   using namespace vertexai::plaidml;
+  using std::unique_ptr;
 }}
 
+cpp_class!(pub unsafe struct Tensor as "unique_ptr<base_tensor>");
+
+cpp_class!(pub unsafe struct Placeholder as "placeholder");
+
+cpp_class!(pub unsafe struct Variable as "variable");
+
+cpp_class!(pub unsafe struct Application as "application");
+
+cpp_class!(pub unsafe struct Function as "function");
+
+cpp_class!(pub unsafe struct Gradient as "gradient");
+
 cpp_class!(pub unsafe struct Device as "device");
+
+
