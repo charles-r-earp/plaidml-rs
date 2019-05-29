ac6667ce92b913c76b1c3b796f147b680ee77a3b
charles-r-earp <charles.r.earp@gmail.com>
2019-05-28 17:04:01 -0700

    Removed ci dir

--- ci/travis/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo $VIRTUAL_ENV
-sudo apt update
-sudo apt install -y python3-pip
-sudo -H pip3 install virtualenv
-virtualenv plaidml
-source plaidml/bin/activate
-pip install plaidml
-source ~/.cargo/env || true
--- ci/travis/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cargo build --examples
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$VIRTUAL_ENV/lib
-cargo run --examples test
